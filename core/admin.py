f8f7b5c7cfe0d405b971faec609b39464a85bd61
Tauya Mtowodzwa <ayuat2003@gmail.com>
2024-06-22 10:25:58 +0200

    Fix department model naming conflict (#2)
    - Rename company.departments field to related_departments
    - Add Department.company with related_name='departments'

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Company, Employee, Department
 
-# Register your models here.
+admin.site.site_header = 'Talent Verify Admin'
+admin.site.site_title = 'Talent Verify Admin Area'
+admin.site.index_title = 'Welcome to the Talent Verify admin area'   
+    
+
+admin.site.register(Company)
+admin.site.register(Employee)
+admin.site.register(Department)
--- core/models.py
+++ core/models.py
@@ -6,10 +6,32 @@ class Company(models.Model):
     registartion_number = models.CharField(max_length=20)
     address = models.TextField()
     contact_person = models.CharField(max_length=100)
-    # departments = models.ManyToManyField('Department', related_name='companies')
+    related_departments = models.ManyToManyField('Department', related_name='companies')
     num_employees = models.IntegerField()
     phone = models.CharField(max_length=20)
     email = models.EmailField()
+
+    def __str__(self):
+        return self.name 
+    
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='departments')
+    head = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+    
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    employee_id = models.CharField(max_length=20)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='employees')
+    role = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    duties = models.TextField()
 
     def __str__(self):
         return self.name
