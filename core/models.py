9250f4a0d8aa894aa66f81af18879c7ec388e389
Tauya Mtowodzwa <ayuat2003@gmail.com>
2024-07-07 12:55:59 +0200

    updated

--- core/models.py
+++ core/models.py
@@ -6,8 +6,7 @@ class Company(models.Model):
     registartion_number = models.CharField(max_length=20)
     address = models.TextField()
     contact_person = models.CharField(max_length=100)
-    related_departments = models.ManyToManyField('Department', related_name='companies')
-    num_employees = models.IntegerField()
+    num_employees = models.IntegerField(default=0)
     phone = models.CharField(max_length=20)
     email = models.EmailField()
 
@@ -15,20 +14,31 @@ class Company(models.Model):
         return self.name 
     
 class Department(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='departments')
 
     def __str__(self):
         return self.name
+      
     
 class Employee(models.Model):
     name = models.CharField(max_length=100)
     employee_id = models.CharField(max_length=20)
     department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='employees')
-    role = models.CharField(max_length=100)
-    start_date = models.DateField()
-    end_date = models.DateField()
-    duties = models.TextField()
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='employees')
+    
 
     def __str__(self):
         return self.name
+    
+
+class Role(models.Model):
+    name = models.CharField(max_length=100)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='roles')
+    duties = models.TextField()
+    start_date = models.DateField(auto_now_add=True, null=True, blank=True)
+    end_date = models.DateField(default=None, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+  
