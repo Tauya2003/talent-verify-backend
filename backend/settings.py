0d4dad1e53f3916e3c016a1be4ebc9388b04716b
Tauya Mtowodzwa <ayuat2003@gmail.com>
2024-07-18 12:30:21 +0200

    Add authuser

--- backend/settings.py
+++ backend/settings.py
@@ -33,7 +33,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
+    
+    'authuser'
 ]
+
+AUTH_USER_MODEL = 'authuser.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
